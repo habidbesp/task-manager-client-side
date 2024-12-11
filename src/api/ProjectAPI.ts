7a959bc568c3dad123af96e70ac0541fcb4b3620
Habid B Espinosa <hab.bad@hotmail.com>
2024-12-11 23:50:33 +0100

    geting projects implemented

--- src/api/ProjectAPI.ts
+++ src/api/ProjectAPI.ts
@@ -1,11 +1,25 @@
 import { api } from "@/lib/axios";
-import { ProjectFormData } from "@/types/index";
+import { dashboardProjectSchema, ProjectFormData } from "@/types/index";
 import { isAxiosError } from "axios";
 
 export async function createProject(formData: ProjectFormData) {
   try {
     const { data } = await api.post("/projects", formData);
     return data;
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error);
+    }
+  }
+}
+
+export async function getProjects() {
+  try {
+    const { data } = await api.get("/projects");
+    const response = dashboardProjectSchema.safeParse(data);
+    if (response.success) {
+      return response.data;
+    }
   } catch (error) {
     if (isAxiosError(error) && error.response) {
       throw new Error(error.response.data.error);
