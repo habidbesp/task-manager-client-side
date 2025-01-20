196db690bc310b5aac91cef06af6ffc4465e69f2
Habid B Espinosa <hab.bad@hotmail.com>
2025-01-20 20:53:31 +0100

    React query dev tools commented out

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import Router from "./router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtoolsPanel } from "@tanstack/react-query-devtools";
+// import { ReactQueryDevtoolsPanel } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
@@ -11,7 +11,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <Router />
-      <ReactQueryDevtoolsPanel />
+      {/* <ReactQueryDevtoolsPanel /> */}
     </QueryClientProvider>
   </StrictMode>
 );
